fe7d16be68d69b0ade46b06e6cb87a1940b11ed3
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2022-11-09 09:04:16 +0200

    2021 day 3

---
+++ cmd/2021/3_1/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+const l = 12
+
+func main() {
+	gamma := getGamma(getInput())
+	epsilon := ^gamma & 0b111111111111
+	fmt.Printf("Gamma:   %8v, %012b\n", gamma, gamma)
+	fmt.Printf("Epsilon: %8v, %012b\n", epsilon, epsilon)
+	fmt.Println("Power consumption:", gamma*epsilon)
+}
+
+func getGamma(input []uint64) (gamma uint64) {
+	onesC := make([]int, l)
+	zeroesC := make([]int, l)
+	for _, ip := range input {
+		for p := 0; p < l; p++ {
+			f := uint64(1 << p)
+			if ip&f == f {
+				onesC[p]++
+			} else {
+				zeroesC[p]++
+			}
+		}
+	}
+
+	for i := 0; i < l; i++ {
+		if onesC[i] > zeroesC[i] {
+			gamma += 1 << i
+		}
+	}
+
+	return
+}
+
+func getInput() []uint64 {
+	lines := strings.Split(input, "\n")
+	m := make([]uint64, len(lines))
+	for i, line := range lines {
+		v, err := strconv.ParseInt(line, 2, 64)
+		if err != nil {
+			panic(err)
+		}
+		m[i] = uint64(v)
+	}
+	return m
+}
---
+++ cmd/2021/3_2/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+//go:embed input.txt
+var input string
+
+func main() {
+	oxygen := getOxygenRating(getInput(), 12)
+	co2 := getCO2Rating(getInput(), 12)
+	fmt.Println("Oxygen:", oxygen)
+	fmt.Println("CO2:", co2)
+	fmt.Println("Life support rating:", oxygen*co2)
+}
+
+func getOxygenRating(input []uint64, l int) uint64 {
+	inputCopy := make([]uint64, len(input))
+	copy(inputCopy, input)
+	return getRating(inputCopy, l, func(onesC, zeroesC int) bool { return onesC >= zeroesC })
+}
+
+func getCO2Rating(input []uint64, l int) uint64 {
+	inputCopy := make([]uint64, len(input))
+	copy(inputCopy, input)
+	return getRating(inputCopy, l, func(onesC, zeroesC int) bool { return onesC < zeroesC })
+}
+
+func getRating(input []uint64, l int, checkF func(onesC, zeroesC int) bool) uint64 {
+	for p := l - 1; p >= 0; p-- {
+		if len(input) == 1 {
+			break
+		}
+
+		mask := uint64(1 << p)
+		onesC := 0
+		zeroesC := 0
+		for _, ip := range input {
+			if ip&mask == mask {
+				onesC++
+			} else {
+				zeroesC++
+			}
+		}
+
+		doPut1 := checkF(onesC, zeroesC)
+
+		var newI []uint64
+		for i := range input {
+			if (input[i]&mask == mask) == doPut1 {
+				newI = append(newI, input[i])
+			}
+		}
+		input = newI
+	}
+
+	if len(input) > 1 {
+		panic(fmt.Sprint(input))
+	}
+
+	return input[0]
+}
+
+func getInput() []uint64 {
+	lines := strings.Split(input, "\n")
+	m := make([]uint64, len(lines))
+	for i, line := range lines {
+		v, err := strconv.ParseInt(line, 2, 64)
+		if err != nil {
+			panic(err)
+		}
+		m[i] = uint64(v)
+	}
+	return m
+}
