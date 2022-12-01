f12acc8292c54615d785f706feb2672159c8df77
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2022-12-01 10:25:22 +0200

    2021 day 5

---
+++ cmd/2021/5_1/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"embed"
+	"errors"
+	"fmt"
+	"github.com/aivarasbaranauskas/aoc/internal/a"
+	"github.com/aivarasbaranauskas/aoc/internal/optimistic"
+	"io"
+	"log"
+	"strings"
+)
+
+//go:embed input.txt
+var inputData embed.FS
+
+func main() {
+	f, err := inputData.Open("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var (
+		maxX, maxY int
+		lines      [][2][2]int
+	)
+
+	s := bufio.NewScanner(f)
+	for s.Scan() {
+		line := s.Text()
+		spl := strings.Split(line, " -> ")
+		splStart := strings.Split(spl[0], ",")
+		splEnd := strings.Split(spl[1], ",")
+		x1 := optimistic.Atoi(splStart[0])
+		y1 := optimistic.Atoi(splStart[1])
+		x2 := optimistic.Atoi(splEnd[0])
+		y2 := optimistic.Atoi(splEnd[1])
+
+		if x1 != x2 && y1 != y2 {
+			// Skip diagonal lines
+			continue
+		}
+
+		maxX = a.Max(maxX, x1, x2)
+		maxY = a.Max(maxY, y1, y2)
+		lines = append(lines, [2][2]int{{x1, y1}, {x2, y2}})
+	}
+	if err = s.Err(); err != nil && !errors.Is(err, io.EOF) {
+		log.Fatalln(err)
+	}
+
+	maxX++
+	maxY++
+	matrix := make([][]int, maxX)
+	for i := 0; i < maxX; i++ {
+		matrix[i] = make([]int, maxY)
+	}
+
+	for _, line := range lines {
+		xFrom, xTo := a.Min(line[0][0], line[1][0]), a.Max(line[0][0], line[1][0])
+		yFrom, yTo := a.Min(line[0][1], line[1][1]), a.Max(line[0][1], line[1][1])
+		for x := xFrom; x <= xTo; x++ {
+			for y := yFrom; y <= yTo; y++ {
+				matrix[x][y]++
+			}
+		}
+	}
+
+	var ct int
+	for x := 0; x < maxX; x++ {
+		for y := 0; y < maxY; y++ {
+			if matrix[x][y] > 1 {
+				ct++
+			}
+		}
+	}
+
+	fmt.Println(ct)
+}
---
+++ internal/a/int.go
@@ -0,0 +1,25 @@
+package a
+
+type Numeric interface {
+	int | int8 | int16 | int32 | int64 | uint | uint8 | uint16 | uint32 | uint64 | float32 | float64
+}
+
+func Max[T Numeric](numbers ...T) (max T) {
+	max = numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		if max < numbers[i] {
+			max = numbers[i]
+		}
+	}
+	return
+}
+
+func Min[T Numeric](numbers ...T) (min T) {
+	min = numbers[0]
+	for i := 1; i < len(numbers); i++ {
+		if min > numbers[i] {
+			min = numbers[i]
+		}
+	}
+	return
+}
