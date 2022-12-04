2c4bec352410f4492580205eb5c8d7983722702b
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2022-12-04 08:55:13 +0200

    2022 day 4

---
+++ cmd/2022/4_1/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"embed"
+	"fmt"
+	"github.com/aivarasbaranauskas/aoc/internal/optimistic"
+	"log"
+	"strings"
+)
+
+//go:embed input.txt
+var inputData embed.FS
+
+func main() {
+	f, err := inputData.Open("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	r := bufio.NewScanner(f)
+	var ct int
+
+	for r.Scan() {
+		line := r.Text()
+		spl := strings.Split(line, ",")
+		spl1 := strings.Split(spl[0], "-")
+		spl2 := strings.Split(spl[1], "-")
+		a := optimistic.Atoi(spl1[0])
+		b := optimistic.Atoi(spl1[1])
+		c := optimistic.Atoi(spl2[0])
+		d := optimistic.Atoi(spl2[1])
+
+		if (a <= c && b >= d) || (c <= a && d >= b) {
+			ct++
+		}
+	}
+
+	fmt.Println(ct)
+}
