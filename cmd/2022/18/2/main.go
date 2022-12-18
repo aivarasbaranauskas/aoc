e098cbf44f9fd949024786af13f0084f1aa89eb8
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2022-12-18 07:25:12 +0200

    2022 day 18

---
+++ cmd/2022/18/2/main.go
@@ -0,0 +1,113 @@
+package main
+
+import (
+	"bufio"
+	"embed"
+	"fmt"
+	"github.com/aivarasbaranauskas/aoc/internal/_a"
+	"github.com/aivarasbaranauskas/aoc/internal/_num"
+	"github.com/aivarasbaranauskas/aoc/internal/_set"
+	"github.com/aivarasbaranauskas/aoc/internal/optimistic"
+	"log"
+	"strings"
+)
+
+//go:embed input.txt
+var inputData embed.FS
+
+func main() {
+	f, err := inputData.Open("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	points := _set.New[[3]int]()
+	var xh, yh, zh [2]int
+
+	r := bufio.NewScanner(f)
+	for r.Scan() {
+		spl := strings.Split(r.Text(), ",")
+		p := [3]int{
+			optimistic.Atoi(spl[0]),
+			optimistic.Atoi(spl[1]),
+			optimistic.Atoi(spl[2]),
+		}
+		points.Add(p)
+		xh[0] = _num.Min(xh[0], p[0])
+		xh[1] = _num.Max(xh[1], p[0])
+		yh[0] = _num.Min(yh[0], p[1])
+		yh[1] = _num.Max(yh[1], p[1])
+		zh[0] = _num.Min(zh[0], p[2])
+		zh[1] = _num.Max(zh[1], p[2])
+	}
+
+	xh[0]--
+	xh[1]++
+	yh[0]--
+	yh[1]++
+	zh[0]--
+	zh[1]++
+
+	visited := _set.New[[3]int]()
+	toVisit := _a.Queue[[3]int]{}
+	toVisit.Enqueue([3]int{xh[0], yh[0], zh[0]})
+	ct := 0
+
+	for !toVisit.Empty() {
+		current := toVisit.Dequeue()
+		if visited.Has(current) {
+			continue
+		}
+		if points.Has(current) {
+			ct++
+			continue
+		}
+		visited.Add(current)
+		if current[0]-1 >= xh[0] {
+			toVisit.Enqueue([3]int{
+				current[0] - 1,
+				current[1],
+				current[2],
+			})
+		}
+		if current[0]+1 <= xh[1] {
+			toVisit.Enqueue([3]int{
+				current[0] + 1,
+				current[1],
+				current[2],
+			})
+		}
+
+		if current[1]-1 >= yh[0] {
+			toVisit.Enqueue([3]int{
+				current[0],
+				current[1] - 1,
+				current[2],
+			})
+		}
+		if current[1]+1 <= yh[1] {
+			toVisit.Enqueue([3]int{
+				current[0],
+				current[1] + 1,
+				current[2],
+			})
+		}
+
+		if current[2]-1 >= zh[0] {
+			toVisit.Enqueue([3]int{
+				current[0],
+				current[1],
+				current[2] - 1,
+			})
+		}
+		if current[2]+1 <= zh[1] {
+			toVisit.Enqueue([3]int{
+				current[0],
+				current[1],
+				current[2] + 1,
+			})
+		}
+	}
+
+	fmt.Println(ct)
+}
