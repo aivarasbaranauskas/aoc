35e151da9a0c3f34eade8c954c740a1d63874457
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2022-12-25 07:23:51 +0200

    2022 day 25

---
+++ cmd/2022/25/2/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"embed"
+	"fmt"
+	"log"
+	"math"
+)
+
+//go:embed input.txt
+var inputData embed.FS
+
+func main() {
+	f, err := inputData.Open("input.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	r := bufio.NewScanner(f)
+	var sum int
+	for r.Scan() {
+		line := r.Text()
+		sum += decode([]byte(line))
+	}
+	fmt.Println(sum)
+	fmt.Println(string(encode(sum)))
+}
+
+func decode(n []byte) (x int) {
+	l := len(n)
+	for i, c := range n {
+		switch c {
+		case '=':
+			x -= int(math.Pow(5, float64(l-i-1))) * 2
+		case '-':
+			x -= int(math.Pow(5, float64(l-i-1)))
+		case '1':
+			x += int(math.Pow(5, float64(l-i-1)))
+		case '2':
+			x += int(math.Pow(5, float64(l-i-1))) * 2
+		}
+	}
+	return
+}
+
+func encode(n int) (x []byte) {
+	var carry int
+	for n > 0 {
+		n += carry
+		carry = 0
+		c := n % 5
+		n = n / 5
+		if c < 3 {
+			x = append([]byte{byte('0' + c)}, x...)
+		} else {
+			carry = 1
+			if c == 3 {
+				x = append([]byte{'='}, x...)
+			} else {
+				x = append([]byte{'-'}, x...)
+			}
+		}
+	}
+	return
+}
