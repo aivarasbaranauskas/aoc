4d03557d34779ab40ad04dbb98b88f7d2561d354
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2025-03-07 09:49:36 +0200

    2024 day 1 optimise

--- solutions/year_2024/1.go
+++ solutions/year_2024/1.go
@@ -1,7 +1,6 @@
 package year_2024
 
 import (
-	"bytes"
 	"github.com/aivarasbaranauskas/aoc/internal/_num"
 	"github.com/aivarasbaranauskas/aoc/internal/optimistic"
 	"sort"
@@ -15,35 +14,38 @@ func init() {
 type Day1 struct{}
 
 func (Day1) Part1(input []byte) string {
-	lines := bytes.Split(input, []byte("\n"))
-	l1, l2 := make([]int, len(lines)), make([]int, len(lines))
-	for i, line := range lines {
-		spl := bytes.Split(line, []byte("   "))
-		l1[i] = optimistic.Atoi(string(spl[0]))
-		l2[i] = optimistic.Atoi(string(spl[1]))
+	var l1, l2 [1000]int
+	for i := 0; i*14 < len(input); i++ {
+		iv := i * 14
+		l1[i] = optimistic.AtoiBFast(input[iv : iv+5])
+		l2[i] = optimistic.AtoiBFast(input[iv+8 : iv+13])
 	}
-	sort.Ints(l1)
-	sort.Ints(l2)
+
+	sort.Ints(l1[:])
+	sort.Ints(l2[:])
+
 	s := 0
 	for i, v := range l1 {
 		s += _num.Abs(v - l2[i])
 	}
+
 	return strconv.Itoa(s)
 }
 
 func (Day1) Part2(input []byte) string {
-	lines := bytes.Split(input, []byte("\n"))
-	l, m := make([]int, len(lines)), make(map[int]int)
-	for i, line := range lines {
-		spl := bytes.Split(line, []byte("   "))
-		l[i] = optimistic.Atoi(string(spl[0]))
-		v2 := optimistic.Atoi(string(spl[1]))
+	var l [1000]int
+	m := make(map[int]int, 1000)
+	for i := 0; i*14 < len(input); i++ {
+		iv := i * 14
+		l[i] = optimistic.AtoiBFast(input[iv : iv+5])
+		v2 := optimistic.AtoiBFast(input[iv+8 : iv+13])
 		if _, ok := m[v2]; ok {
 			m[v2]++
 		} else {
 			m[v2] = 1
 		}
 	}
+
 	s := 0
 	for _, v := range l {
 		if freq, ok := m[v]; ok {
