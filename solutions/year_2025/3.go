d8baa5a3c5d2d53fa3480da2cbb7a70f1c1d9738
Aivaras Baranauskas <aiarasb@gmail.com>
2025-12-03 16:31:33 +0200

    2025 day 3 but faster

--- solutions/year_2025/3.go
+++ solutions/year_2025/3.go
@@ -2,10 +2,9 @@ package year_2025
 
 import (
 	"bytes"
-	"slices"
 	"strconv"
 
-	"github.com/aivarasbaranauskas/aoc/internal/_slice"
+	"github.com/aivarasbaranauskas/aoc/internal/optimistic"
 )
 
 func init() {
@@ -15,12 +14,22 @@ func init() {
 type Day3 struct{}
 
 func (Day3) Part1(input []byte) string {
-	m := bytes.Split(input, []byte("\n"))
 	sum := 0
 
-	for _, line := range m {
-		d1i, d1 := _slice.MaxI(line[:len(line)-1])
-		d2 := slices.Max(line[d1i+1:])
+	for i := 0; i < len(input); i++ {
+		d1 := input[i]
+		d2 := input[i+1]
+		i++
+
+		for ; i < len(input) && input[i] != '\n'; i++ {
+			if input[i] > d1 && i+1 < len(input) && input[i+1] != '\n' {
+				d1 = input[i]
+				d2 = input[i+1]
+			} else if input[i] > d2 {
+				d2 = input[i]
+			}
+		}
+
 		sum += int(d1-'0')*10 + int(d2-'0')
 	}
 
@@ -28,17 +37,43 @@ func (Day3) Part1(input []byte) string {
 }
 
 func (Day3) Part2(input []byte) string {
-	m := bytes.Split(input, []byte("\n"))
 	sum := 0
 
-	for _, line := range m {
-		num := 0
-		for i := 0; i < 12; i++ {
-			id, d := _slice.MaxI(line[:len(line)-(11-i)])
-			line = line[id+1:]
-			num = num*10 + int(d-'0')
+	for len(input) > 0 {
+		var buf [12]byte
+
+		lineEnd := bytes.IndexByte(input, '\n')
+		if lineEnd == -1 {
+			lineEnd = len(input)
 		}
-		sum += num
+
+	OuterLoop:
+		for i := 0; i < lineEnd; i++ {
+			for j := 0; j < 12; j++ {
+				if 12-j > lineEnd-i {
+					continue
+				}
+				if buf[j] == 0 {
+					buf[j] = input[i]
+					continue OuterLoop
+				}
+				if input[i] > buf[j] {
+					buf[j] = input[i]
+					for j2 := j + 1; j2 < 12; j2++ {
+						buf[j2] = 0
+					}
+					continue OuterLoop
+				}
+			}
+		}
+
+		sum += optimistic.AtoiBFast(buf[:])
+
+		if lineEnd < len(input) {
+			input = input[lineEnd+1:]
+		} else {
+			input = input[lineEnd:]
+		}
 	}
 
 	return strconv.Itoa(sum)
