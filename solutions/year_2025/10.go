ba4bb0c31e8549f9598f1d81559f6df98b8d4089
Aivaras Baranauskas <aiarasb@gmail.com>
2025-12-14 20:30:53 +0200

    2025 day 10 p2

--- solutions/year_2025/10.go
+++ solutions/year_2025/10.go
@@ -2,7 +2,6 @@ package year_2025
 
 import (
 	"bytes"
-	"cmp"
 	"strconv"
 
 	"github.com/aivarasbaranauskas/aoc/internal/_a"
@@ -68,8 +67,8 @@ func (day Day10) Part2(input []byte) string {
 	lines := bytes.Split(input, []byte{'\n'})
 
 	sum := 0
-	for i := range lines {
-		sum += day.configureJoltage(lines[i])
+	for _, v := range lines {
+		sum += day.configureJoltage(v)
 	}
 
 	return strconv.Itoa(sum)
@@ -88,58 +87,86 @@ func (day Day10) configureJoltage(input []byte) int {
 	spl := bytes.Split(input, []byte{' '})
 	goal := parse(spl[len(spl)-1])
 	buttons := make([][]int, len(spl)-2)
+	buttonsI := make([]int, len(spl)-2)
 	for i := 1; i < len(spl)-1; i++ {
 		buttons[i-1] = parse(spl[i])
-	}
-
-	type S struct {
-		joltage              []int
-		lastI, buttonPresses int
-	}
-
-	var q _a.Queue[S]
-	q.Enqueue(S{joltage: make([]int, len(goal))})
-
-	pushButton := func(joltage []int, button []int) []int {
-		newJoltage := make([]int, len(joltage))
-		copy(newJoltage, joltage)
-		for _, i := range button {
-			newJoltage[i]++
+		for _, v := range buttons[i-1] {
+			buttonsI[i-1] |= 1 << v
 		}
-		return newJoltage
 	}
+
+	combos := map[int][]int{0: {0}} // buttons masks
+	var q _a.Queue[[4]int]          // [current state, last clicked button i, buttons presses count, buttons mask]
+	q.Enqueue([4]int{0, -1, 0, 0})
 
 	for !q.Empty() {
 		state := q.Dequeue()
 
-	OuterLoop:
-		for i := state.lastI; i < len(buttons); i++ {
-			s := pushButton(state.joltage, buttons[i])
+		for i := state[1] + 1; i < len(buttons); i++ {
+			s := state[0] ^ buttonsI[i]
+			c := state[2] + 1
+			mask := state[3] | (1 << i)
+			q.Enqueue([4]int{s, i, c, mask})
 
-			eq := true
-			for j := range s {
-				if c := cmp.Compare(s[j], goal[j]); c != 0 {
-					if c > 0 {
-						// got past the goal, terminate
-						continue OuterLoop
-					}
-					eq = false
-				}
+			if cc, ok := combos[s]; ok {
+				combos[s] = append(cc, mask)
+			} else {
+				combos[s] = []int{mask}
 			}
-
-			if eq {
-				return state.buttonPresses + 1
-			}
-
-			// BFS won't do ehe - it's a ILP problem... TODO
-
-			q.Enqueue(S{
-				joltage:       s,
-				lastI:         i,
-				buttonPresses: state.buttonPresses + 1,
-			})
 		}
 	}
 
-	panic("not found")
+	var findMin func([]int, int, int) (int, bool)
+
+	findMin = func(goal []int, ct, mul int) (int, bool) {
+		allZero := true
+		for _, v := range goal {
+			allZero = allZero && (v == 0)
+		}
+		if allZero {
+			return ct, true
+		}
+
+		goalMask := 0
+		for i, v := range goal {
+			goalMask |= (v % 2) << i
+		}
+
+		matchingCombos, ok := combos[goalMask]
+		if !ok {
+			return 0, false
+		}
+
+		goalTmp := make([]int, len(goal))
+		found := false
+		minV := 1 << 30
+		for _, buttonsMask := range matchingCombos {
+			copy(goalTmp, goal)
+			ctTmp := ct
+			for i, button := range buttons {
+				if buttonsMask&(1<<i) > 0 {
+					ctTmp += mul
+					for _, j := range button {
+						goalTmp[j]--
+					}
+				}
+			}
+			for i := range goalTmp {
+				goalTmp[i] /= 2
+			}
+			rez, ok := findMin(goalTmp, ctTmp, mul*2)
+			if ok {
+				found = true
+				minV = min(minV, rez)
+			}
+		}
+		return minV, found
+	}
+
+	ct, ok := findMin(goal, 0, 1)
+	if !ok {
+		panic("not found")
+	}
+
+	return ct
 }
